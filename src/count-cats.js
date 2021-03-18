a902c6e88ffff22c89cf23ca223e4a0a313d413d
VeronikaKlejmyonova <veronika.klejmyonova@gmail.com>
2021-03-18 21:04:23 +0500

    feat: add solving the 'Count cats' task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
+//const matrix = ([
+//    ['##', 'dd', '00'],
+//    ['^', '^', 'ss'],
+//    ['AA', 'dd', 'Oo'],
+//]);
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix /* backyard*/ ) {
+    //throw new CustomError('Not implemented');
+    // remove line with error and write your code here
+    //  let number = matrix.filter(x => x == '^^');
+    //   return number.length;
+    let num = 0;
+    let arr = matrix.concat(...matrix);
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "^^")
+            num += 1;
+    }
+    return num;
+    //    while (matrix.indexOf('^^') != -1) {
+    //      num += 1;
+    //  }
 };
